6d583c7f9f06bbfa5c3d1da3813b1fd999507cd1
TGKElit <tgkelit@gmail.com>
2022-12-08 09:35:57 +0100

    plus-minus sign togglable

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 for(let idNum = 0; idNum < 4; idNum ++) {
     document.getElementById("question-" + idNum).addEventListener("click", function(){
         document.getElementById("answer-" + idNum).classList.toggle("hidden");
+        document.getElementById("plus-" + idNum).classList.toggle("hidden");
+        document.getElementById("minus-" + idNum).classList.toggle("hidden");
     });
+
 }
+
